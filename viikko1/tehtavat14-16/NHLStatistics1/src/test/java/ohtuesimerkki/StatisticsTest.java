ad055ffcf1486979345a747d70ad5e0265a5b3ea
Marcestus <55045740+Marcestus@users.noreply.github.com>
2021-01-25 18:20:40 +0200

    lisää viikkotehtävien 14-16 vastaukset

---
+++ viikko1/tehtavat14-16/NHLStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marivaan
+ */
+public class StatisticsTest {
+    
+    public StatisticsTest() {
+    }
+    
+    Reader readerStub = new Reader() {
+ 
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<>();
+ 
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+ 
+            return players;
+        }
+    };
+ 
+    Statistics stats;
+
+    @Before
+    public void setUp(){
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }
+    
+    @Test
+    public void playerCanBeFound() {
+        assertEquals("Semenko", stats.search("Semenko").getName());
+    }
+    
+    @Test
+    public void playerCantBeFound() {
+        assertEquals(null, stats.search("Semko"));
+    }
+    
+    @Test
+    public void playerIsInCorrectTeam() {
+        assertEquals(1, stats.team("PIT").size());
+        assertEquals(3, stats.team("EDM").size());
+    }
+    
+    @Test
+    public void topScorersCorrectOrder() {
+        assertEquals("Gretzky", stats.topScorers(1).get(0).getName());
+        assertEquals("Yzerman", stats.topScorers(2).get(2).getName());
+    }
+    
+}
